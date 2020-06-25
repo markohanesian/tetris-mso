c92e19c87bcc51c0369103e4328a6bd84f591331
Mark <mso872@gmail.com>
2020-06-25 16:45:03 -0700

    added function control to move left, right, rotate

--- app.js
+++ app.js
@@ -71,7 +71,21 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
     //   make the tetromino move down every second
-    timerId = setInterval(moveDown, 1000)
+    timerId = setInterval(moveDown, 400)
+
+    // assign functions to keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+          moveLeft()
+        } else if (e.keyCode === 38) {
+          rotate()
+        } else if (e.keyCode === 39) {
+          moveRight()
+        } else if (e.keyCode === 40) {
+          moveDown()
+        }
+      }
+    document.addEventListener('keyup', control)
 
     // move down function
     function moveDown() {
@@ -93,6 +107,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // rules to move tetromino left unless is at the edge or there is blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0 )
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition+=1
+        }
+    }
 })
 
 
