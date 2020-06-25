ee72a039b2eaae71bb3d8391c8d3d7f8f5fb01fa
Mark <mso872@gmail.com>
2020-06-25 11:10:26 -0700

    added grid function

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // all javascript must be written wrapped in this event listener
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
 
+    console.log(squares)
 })
+
+
