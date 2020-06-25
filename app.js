825d2277e9552365b913f2106d521a713ef23b5e
Mark <mso872@gmail.com>
2020-06-25 16:31:14 -0700

    added freeze() to stop tetrimones when at bottom of grid

--- app.js
+++ app.js
@@ -78,7 +78,21 @@ document.addEventListener('DOMContentLoaded', () => {
         undraw()
         currentPosition += width
         draw()
+        freeze()
     }
+
+    // freeze function
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            // start a new tetromino falling
+            random = Math.floor(Math.random() * theTetriminoes.length)
+            current = theTetriminoes[random] [currentRotation]
+            currentPosition = 4
+            draw() 
+        }
+    }
+
 })
 
 
