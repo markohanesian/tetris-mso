d6d5bd14b6265506c37805c9a94ed122fe7646d4
Mark <mso872@gmail.com>
2020-06-25 17:09:44 -0700

    added function to remove tetromino from displaying in grid

--- app.js
+++ app.js
@@ -157,6 +157,19 @@ document.addEventListener('DOMContentLoaded', () => {
         [0, 1, displayWidth, displayWidth+1], //oTetromino
         [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
       ]
+
+    //   display the shape in the mini-grid display
+    function displayShape() {
+        //remove any trace of a tetromino form the entire grid
+        displaySquares.forEach(square => {
+          square.classList.remove('tetromino')
+          square.style.backgroundColor = ''
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+          displaySquares[displayIndex + index].classList.add('tetromino')
+          displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
+        })
+      }
 })
 
 
